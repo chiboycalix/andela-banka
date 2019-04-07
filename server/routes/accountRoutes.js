c41e7b6454e95b525f7424bf600cece0b83b803f
chiboycalix <igwechinonso77@gmail.com>
2019-04-07 14:47:41 +0100

    Created Admin/Staff activate/deactivate functionality Only a staff or admin can perform this operation [start #165167917]

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AccountController from '../controllers/AccountController';
 import isUser from '../middlewares/isUser';
+import isStaff from '../middlewares/isStaff';
 
 
 const router = express.Router();
 
 router.post('/', isUser, AccountController.createAccount);
+router.patch('/:accountNum', isStaff, AccountController.patchAccount);
 
 export default router;
