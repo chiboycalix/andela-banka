e31ea649b9c7e3f2a39c0570857385d5c5b5c5ee
chiboycalix <igwechinonso77@gmail.com>
2019-04-07 09:35:34 +0100

    ch(dev-env-setup): Installed necessary dependencies Integrated travis ci, coveralls and code coverage

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-console":0,
+    "consistent-return": 0,
+    "array-callback-return": 0,
+    "import/no-extraneous-dependencies":0,
+    "no-undef":0
+  },
+};
