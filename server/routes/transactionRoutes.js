64591bfbdb84009dc59295b1a9d48b537ec35d00
chiboycalix <igwechinonso77@gmail.com>
2019-04-18 00:52:27 +0100

    heroku app

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import TransactionController from '../controllers/TransactionController';
 import isStaff from '../middlewares/isStaff';
 import isValidTransaction from '../middlewares/debitValidation';
+import accountValidations from '../middlewares/accountValidations';
 
 const router = express.Router();
 
-router.post('/:accountNum/debit', isStaff, isValidTransaction, TransactionController.debitAccount);
-router.post('/:accountNum/credit', isStaff, TransactionController.creditAccount);
-router.get('/:accountNum/transactions', TransactionController.getAllTransactions);
-router.get('/:accountNum/transactions/:transactionsId', TransactionController.getTransaction);
+
+router.post('/:accountNum/debit', isStaff, accountValidations, isValidTransaction, TransactionController.debitAccount);
+router.post('/:accountNum/credit', isStaff, accountValidations, TransactionController.creditAccount);
+router.get('/:accountNum/transactions', accountValidations, TransactionController.getAllTransactions);
+router.get('/:accountNum/transactions/:transactionsId', accountValidations, TransactionController.getTransaction);
 
 export default router;
