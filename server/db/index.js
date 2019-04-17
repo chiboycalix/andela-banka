d37d2ca0b387ac9ec7d53f45518dd1558971c9ed
chiboycalix <igwechinonso77@gmail.com>
2019-04-17 11:09:10 +0100

    Added code to accomodate test environment

--- server/db/index.js
+++ server/db/index.js
@@ -8,10 +8,12 @@ const env = process.env.NODE_ENV;
 console.log(env);
 let connection;
 
-if (env === 'development') {
-  connection = connectionString.development;
-} else if (env === 'production') {
+if (env === 'production') {
   connection = connectionString.production;
+} else if (env === 'test') {
+  connection = connectionString.test;
+} else {
+  connection = connectionString.development;
 }
 
 const pool = new Pool(connection);
