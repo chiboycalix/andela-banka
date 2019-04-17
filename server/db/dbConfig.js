8983f8b79f5e67fd82a08b41bbbd9e66f7449d8b
chiboycalix <igwechinonso77@gmail.com>
2019-04-17 11:05:49 +0100

    Added code to accomodate test environment

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -12,6 +12,13 @@ connectionString.development = {
   port: process.env.DB_PORT,
 };
 
+connectionString.test = {
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+};
 
 connectionString.production = {
   user: process.env.USER,
