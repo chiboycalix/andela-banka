aae6d37a6ca22bbba03fd9dc3cd2e50402918e62
chiboycalix <igwechinonso77@gmail.com>
2019-04-19 12:54:11 +0100

    Persisted debit account endpoint [start #165463616]

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import TransactionController from '../controllers/TransactionController';
-import isStaff from '../middlewares/isStaff';
-import isValidTransaction from '../middlewares/debitValidation';
+// import isStaff from '../middlewares/isStaff';
+import isUser from '../middlewares/isUser';
+// import isValidTransaction from '../middlewares/debitValidation';
 
 const router = express.Router();
 
-router.post('/:accountNum/debit', isStaff, isValidTransaction, TransactionController.debitAccount);
-router.post('/:accountNum/credit', isStaff, TransactionController.creditAccount);
+router.post('/:accountNum/debit', isUser, TransactionController.debitAccount);
+router.post('/:accountNum/credit', isUser, TransactionController.creditAccount);
 router.get('/:accountNum/transactions', TransactionController.getAllTransactions);
 router.get('/:accountNum/transactions/:transactionsId', TransactionController.getTransaction);
 
