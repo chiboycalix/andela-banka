f28cd41ae770b7213b39cfa773502a71fba24c8e
chiboycalix <igwechinonso77@gmail.com>
2019-04-24 02:52:14 +0100

    bcrypt fix

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import User from '../queryhelpers/userQuery';
 
 dotenv.config();
