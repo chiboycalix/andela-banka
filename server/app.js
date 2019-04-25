22ad488f4ec84a2dfee4178dfdafb0cc32c9fe21
chiboycalix <igwechinonso77@gmail.com>
2019-04-25 19:44:19 +0100

    API documentation with Swagger-ui-express

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
 
 
+
 const app = express();
+const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes(app);
 
