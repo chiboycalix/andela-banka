f1c35d1584c3d486ef0493dd83b3f0c7af6994a0
chiboycalix <igwechinonso77@gmail.com>
2019-05-01 05:49:24 +0100

    fixing cors

--- server/app.js
+++ server/app.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import debug from 'debug';
 import routes from './routes/routes';
 import swaggerUI from 'swagger-ui-express';
 import swaggerdoc from './../swagger';
 
 
 const app = express();
+const debugg = debug('app');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.options('*', cors());
 app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerdoc));
 routes(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Application listening on port ${PORT}`);
+  debugg(`Application listening on port ${PORT}`);
 });
 
 
